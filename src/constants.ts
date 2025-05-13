3df696f05a069c6d36775b68aedbb06ed82fdb2a
Daniel Zhu <danielfangzhu@gmail.com>
2025-05-12 22:05:20 -0700

    add giscus

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import type { GiscusProps } from "@giscus/react";
+
+export const GISCUS: GiscusProps = {
+  repo: "xyzqm/blog",
+  repoId: "R_kgDONtlFAA",
+  category: "Announcements",
+  categoryId: "DIC_kwDONtlFAM4CqFQ8",
+  mapping: "pathname",
+  reactionsEnabled: "1",
+  emitMetadata: "0",
+  inputPosition: "bottom",
+  lang: "en",
+  loading: "lazy",
+};
